583131a3b6a25463c721d4c69834bbad474fb4ab
suzannahurst <hurstsuzanna@gmail.com>
2022-11-02 13:37:07 +0000

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useRef } from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import Landing from "./pages/landing/landing";
+// import Landing from "./pages/landing/landing";
 import Home from "./pages/home/home";
 import Book from "./pages/book/book";
 import Contact from "./pages/contact/contact";
 import Expect from "./pages/expect/expect";
 import About from "./pages/about/about";
 import Pricing from "./pages/pricing/pricing";
-import NotFound from "./pages/404/404";
+// import NotFound from "./pages/404/404";
 import Header from "./components/Structure/Header/Header";
 import Main from "./components/Structure/Main/Main";
 import Footer from "./components/Structure/Footer/Footer";
