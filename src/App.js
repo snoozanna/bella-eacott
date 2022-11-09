b79da108a698c3ca7c7d4f8aba2448ce3afaf5ac
suzannahurst <hurstsuzanna@gmail.com>
2022-11-09 11:56:41 +0000

    holding page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import { BrowserRouter as Router } from "react-router-dom";
-import Landing from "./pages/landing/landing";
+// import Landing from "./pages/landing/landing";
+import Holding from "./pages/holding/holding";
 import NotFound from "./pages/404/404";
-import Wrapper from "./components/Structure/Wrapper/Wrapper";
+// import Wrapper from "./components/Structure/Wrapper/Wrapper";
 
 import "./App.css";
 import "./styles/generics.css";
@@ -11,8 +12,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/landing" element={<Landing />} />
-        <Route index element={<Wrapper />} />
+        {/* <Route path="/landing" element={<Landing />} /> */}
+        <Route index element={<Holding />} />
+        {/* <Route index element={<Wrapper />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
