8130d1babeafe1c1103d0d63210d1a484f0cd1be
suzannahurst <hurstsuzanna@gmail.com>
2022-11-16 16:33:22 +0000

    form attempt

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -55,28 +55,25 @@ export const Contact = ({ initialValues }) => {
         </p>
         <form
           name="contact"
-          method="POST"
-          data-netlify="true"
+          method="post"
+          netlify="true"
           netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
           <p>
-            <label>
-              Name: <input type="text" name="name" />
-            </label>
+            <label htmlFor="name">Name</label> <br />
+            <input type="text" id="name" name="name" required />
           </p>
           <p>
-            <label>
-              Email: <input type="email" name="email" />
-            </label>
+            <label htmlFor="email">Email</label> <br />
+            <input type="email" id="email" name="email" required />
           </p>
           <p>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
+            <label htmlFor="message">Message</label> <br />
+            <textarea id="message" name="message" required></textarea>
           </p>
           <p>
-            <button type="submit">Send</button>
+            <input type="submit" value="Submit message" />
           </p>
         </form>
         {/* <form
