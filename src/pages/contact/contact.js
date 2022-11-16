5be514255b1ae96aad78f6255835bfbce730fd1e
suzannahurst <hurstsuzanna@gmail.com>
2022-11-16 17:16:33 +0000

    try form

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,11 +1,5 @@
-import {
-  useNetlifyForm,
-  NetlifyFormProvider,
-  NetlifyFormComponent,
-  Honeypot,
-} from "react-netlify-forms";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+// import { useState } from "react";
 // import { yupResolver } from "@hookform/resolvers/yup";
 // import * as yup from "yup";
 import "./contact.css";
@@ -18,86 +12,35 @@ import "./contact.css";
 // });
 
 export const Contact = ({ initialValues }) => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({ mode: "onBlur" });
-  const netlify = useNetlifyForm({
-    name: "react-hook-form",
-    action: "/thanks",
-    honeypotName: "bot-field",
-    onSuccess: (response, context) => {
-      console.log("Successfully sent form data to Netlify Server");
-    },
-  });
-  const onSubmit = (data) => netlify.handleSubmit(null, data);
-
-  const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i;
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   reset,
+  //   formState: { errors },
+  // } = useForm({ mode: "onBlur" });
 
   return (
-    <NetlifyFormProvider {...netlify}>
-      <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)}>
-        <Honeypot />
-        {netlify.success && (
-          <p sx={{ variant: "alerts.success", p: 3 }}>
-            Thanks for contacting us!
-          </p>
-        )}
-        {netlify.error && (
-          <p sx={{ variant: "alerts.muted", p: 3 }}>
-            Sorry, we could not reach servers. Because it only works on Netlify,
-            our GitHub demo does not provide a response.
-          </p>
-        )}
-        {/* <div>
-          <label htmlFor="name">Name</label> <br />
-          <input type="text" id="name" name="name" required />
-        </div> */}
-        <div>
-          <label htmlFor="email" sx={{ variant: "forms.label" }}>
-            Email:
-          </label>
-
-          <input
-            type="email"
-            id="email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: EMAIL_REGEX,
-                message: "Invalid email address",
-              },
-            })}
-            sx={{
-              variant: "forms.input",
-            }}
-          />
-          {errors.email && (
-            <div sx={{ variant: "text.error" }}>{errors.email.message}</div>
-          )}
-        </div>
-        {/* <div>
-          <p>
-            <label htmlFor="message">Message</label> <br />
-            <textarea id="message" name="message" required></textarea>
-          </p>
-        </div> */}
-        <div sx={{ pt: 3 }}>
-          <button type="submit" sx={{ variant: "buttons.success" }}>
-            Submit
-          </button>
-          <button
-            type="reset"
-            onClick={() => reset()}
-            sx={{ variant: "buttons.danger" }}
-          >
-            Reset
-          </button>
-        </div>
-      </NetlifyFormComponent>
-    </NetlifyFormProvider>
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
   // const [populated, setPopulated] = useState(false);
   // const defaultValues = {
